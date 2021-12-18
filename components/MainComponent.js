b3df7fa94fe22d671d885b508252108a6afc7d07
Thiago Simonetti <tbtsimon7@gmail.com>
2021-12-18 13:08:59 -0800

    About and Contact Page

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,11 +2,30 @@ import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 import Constants from "expo-constants";
 import { View, Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -27,9 +46,26 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
-const HomeNavigator = createStackNavigator(
+const AboutNavigator = createStackNavigator(
   {
-    Home: { screen: Home },
+    About: { screen: About },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact },
   },
   {
     defaultNavigationOptions: {
@@ -48,6 +84,8 @@ const MainNavigator = createDrawerNavigator(
   {
     Home: { screen: HomeNavigator },
     Directory: { screen: DirectoryNavigator },
+    About: { screen: AboutNavigator },
+    Contact: { screen: ContactNavigator },
   },
   {
     drawerBackgroundColor: "#CEC8FF",
